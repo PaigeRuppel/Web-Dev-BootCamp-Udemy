a8b333bfec29ac2eaf489b110960b10bc079fd12
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-16 13:29:53 -0400

    added basic functionality in example get methods

--- intro-to-express/first-express-app/app.js
+++ intro-to-express/first-express-app/app.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const app = express();
+
+// "/" => "Hi There!" -- req and res are objects with request and response info
+app.get("/", function(req, res) {
+	res.send("Hi there!");
+}); 
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+	res.send("Goodbye!!");
+});
+// "/dog" => "Meow"
+app.get("/dog", function(req, res){
+	res.send("MEOW");
+})
+
+app.listen(3000, function(){
+	console.log("Serving demo app on port 3000");
+});
