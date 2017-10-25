b2370a6cd19c7b57d3068691a697d0c8f15ffe00
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-25 09:02:39 -0400

    included partials in ejs views, added use and set statements to app.js

--- more-express/ejs-demo/app.js
+++ more-express/ejs-demo/app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 //root
 app.get("/", function(req, res) {
-	res.render("home.ejs");
+	res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
 	var thing = req.params.thing;
-	res.render("love.ejs", {thingVar: thing});	
+	res.render("love", {thingVar: thing});	
 });
 
 app.get("/posts", function(req, res) {
@@ -17,7 +20,7 @@ app.get("/posts", function(req, res) {
 	{title: "Catnip Dreams", author: "Paige"},
 	{title: "Best canned food according to cats", author: "Paige"}
 	]
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
 });
 
 app.listen(4000, function(){
