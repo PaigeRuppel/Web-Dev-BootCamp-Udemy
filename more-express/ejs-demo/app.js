d593ac5d522952bdda4974d13cea73744151e1e4
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-25 08:26:26 -0400

    practice with ejs - conditionals and looping

--- more-express/ejs-demo/app.js
+++ more-express/ejs-demo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function(req, res) {
 	var thing = req.params.thing;
 	res.render("love.ejs", {thingVar: thing});	
 });
+
+app.get("/posts", function(req, res) {
+	var posts = [
+	{title: "Post 1", author: "Paige"},
+	{title: "Catnip Dreams", author: "Paige"},
+	{title: "Best canned food according to cats", author: "Paige"}
+	]
+	res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(4000, function(){
 	console.log("server is listening on port 4000");
 });
