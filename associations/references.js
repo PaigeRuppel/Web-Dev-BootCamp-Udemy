8c93712df9d55f3e69b784efd408a118fc348494
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-13 09:08:41 -0500

    cleaned up references examples with modules

--- associations/references.js
+++ associations/references.js
@@ -1,51 +1,34 @@
 var mongoose = require("mongoose");
 
 
-
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/blog_demo_2", {useMongoClient:true});
 
-//POST = title, content
-var postSchema = new mongoose.Schema({
-	title: String,
-	content: String
-});
 
-var Post = mongoose.model("Post", postSchema);
+var Post = require("./models/post");
+var User = require("./models/user");
 
-//USER = email, name
-var userSchema = new mongoose.Schema({
-	email: String,
-	name: String,
-	posts: [		
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Post"
+
+
+Post.create({
+	title: "How to cook the best burger",
+	content: "I guess this is more relevant"
+}, function(err,post){
+	User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+		if(err) {
+			console.log(err);
+		} else {
+			foundUser.posts.push(post);
+			foundUser.save(function(err, data){
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(data);
+				}
+			});
 		}
-	]
+	});
 });
-
-var User = mongoose.model("User", userSchema);
-
-// Post.create({
-// 	title: "How to cook the best steak Part 3",
-// 	content: "Just don't cook it to death"
-// }, function(err,post){
-// 	User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-// 		if(err) {
-// 			console.log(err);
-// 		} else {
-// 			foundUser.posts.push(post);
-// 			foundUser.save(function(err, data){
-// 				if (err) {
-// 					console.log(err);
-// 				} else {
-// 					console.log(data);
-// 				}
-// 			});
-// 		}
-// 	});
-// });
 
 // User.create({
 // 	email: "bob@gmail.com",
@@ -55,10 +38,10 @@ var User = mongoose.model("User", userSchema);
 //Find user
 //Find all posts for that user
 
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
-	if(err) {
-		console.log(err);
-	} else {
-		console.log(user);
-	}
-});
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(user);
+// 	}
+// });
